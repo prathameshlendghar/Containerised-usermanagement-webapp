69523fc80ba13bb42c0edfc7d8b34604c207f7e6
Prathamesh lendghar <prathameshlendghar78@gmail.com>
2024-08-22 00:57:38 +0530

    Create new Project and new app Link App inside project

---
+++ CRUD_webapp/employee_records_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EmployeeRecordsManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'employee_records_management'
