28156b382ff4d618315ad3455f5baf3f612fca53
Prathamesh lendghar <prathameshlendghar78@gmail.com>
2024-08-21 13:12:57 +0530

    Create new application in Django

--- DemoProj/DemoProj/urls.py
+++ DemoProj/DemoProj/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 from DemoProj.views import Hello_geeks_func
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("demoApp.urls")),
     path('geeks/', Hello_geeks_func),
 ]
